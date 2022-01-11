bf78b1b1b7a8043d24f33ef4002a167cd752c43a
Samarth1002 <samarthanand622001@gmail.com>
2022-01-11 09:39:19 +0530

    Added main heading

---
+++ src/Components/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <>
+      <div className=" text-center main-heading my-4 py-3 ">
+        Angry || Hungry || Dazzy || Fizzy
+      </div>
+
+      <div className="text-center main-heading ">Just search Here!</div>
+    </>
+  );
+};
+
+export default Title;
